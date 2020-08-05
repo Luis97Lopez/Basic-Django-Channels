a59d68e7e3f88f67c401d7bc6e78d5ec1020f681
luislpzr <lpzr97@gmail.com>
2020-08-05 14:33:38 -0500

    Añadimos redis al proyecto

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -84,6 +84,16 @@ DATABASES = {
     }
 }
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("localhost", 6379)],
+            # "hosts": [os.environ.get('REDIS_URL, 'redis://localhost:6379')]
+        },
+    },
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
